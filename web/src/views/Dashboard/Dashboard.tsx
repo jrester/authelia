5d4c8be87c7f9896e8f67cd377720365552277d8
jrester <jrester379@gmail.com>
2021-08-22 13:13:06 +0200

    Create basic Scaffold

--- web/src/views/Dashboard/Dashboard.tsx
+++ web/src/views/Dashboard/Dashboard.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
-import { Typography, Grid, AppBar, Toolbar, IconButton, Drawer, Divider } from "@material-ui/core";
+import { Typography, Grid, AppBar, Toolbar, IconButton, Drawer, Divider, Box } from "@material-ui/core";
 import { createStyles, makeStyles, useTheme, Theme } from "@material-ui/core/styles";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import MenuIcon from "@material-ui/icons/Menu";
+import {
+    DashboardOutlined,
+    MenuOutlined,
+    PeopleOutlined,
+    SecurityOutlined,
+    SettingsOutlined,
+} from "@material-ui/icons";
 import clsx from "clsx";
 
 import AutheliaTitleIcon from "@assets/images/authelia-title.png";
 
-const drawerWidth = 240;
+const drawerWidth = 150;
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -73,13 +78,29 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(3),
         },
         iconContainer: {
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: 10,
+            margin: theme.spacing(7),
             width: drawerWidth,
         },
         icon: {
-            width: "-webkit-fill-available",
+            width: "inherit",
+        },
+        drawerElement: {
+            color: "rgba(0,0,0,0.6)",
+            display: "flex",
+            alignItems: "center",
+            marginLeft: theme.spacing(2),
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(1),
+            cursor: "pointer",
+        },
+        drawerElementActive: {
+            borderRight: `4px solid ${theme.palette.primary.main}`,
+            color: `${theme.palette.primary.main}`,
+        },
+        drawerElementIcon: {
+            marginRight: theme.spacing(1),
+            width: "1.5em",
+            height: "1.5em",
         },
     }),
 );
@@ -91,33 +112,64 @@ enum Page {
     Config = "Config",
 }
 
+const pageIcons = {
+    [Page.Dashboard]: DashboardOutlined,
+    [Page.Users]: PeopleOutlined,
+    [Page.ACL]: SecurityOutlined,
+    [Page.Config]: SettingsOutlined,
+};
+
 export interface DrawerElementProps {
     page: Page;
     active: boolean;
+    onClick: MouseEventHandler;
 }
 
 const DrawerElement = function (props: DrawerElementProps) {
-    const iconForElement = () => {
-        switch (props.page) {
-            case Page.Dashboard:
-                return DashboardIcon;
-            case Page.Users:
-            case Page.ACL:
-            case Page.Config:
-        }
-    };
+    const theme = useTheme();
+    const classes = useStyles(theme);
+
+    const Icon = pageIcons[props.page];
+
     return (
-        <div>
-            <Typography variant="h2">{Page[props.page]}</Typography>
+        <div
+            className={clsx(classes.drawerElement, { [classes.drawerElementActive]: props.active })}
+            onClick={props.onClick}
+        >
+            <Icon className={clsx(classes.drawerElementIcon)} />
+            <Typography variant="h4">{Page[props.page]}</Typography>
         </div>
     );
 };
 
+const DashboardPage = () => {
+    return <Box />;
+};
+
+const UsersPage = () => {
+    return <Box />;
+};
+
+const ACLPage = () => {
+    return <Box />;
+};
+
+const ConfigPage = () => {
+    return <Box />;
+};
+
+const pages = {
+    [Page.Dashboard]: DashboardPage,
+    [Page.Users]: UsersPage,
+    [Page.ACL]: ACLPage,
+    [Page.Config]: ConfigPage,
+};
+
 const Dashboard = function () {
     const theme = useTheme();
-    const classes = useStyles();
+    const classes = useStyles(theme);
     const [open, setOpen] = React.useState(false);
-    const [content, setContent] = React.useState(Page.Dashboard);
+    const [page, setPage] = React.useState(Page.Dashboard);
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -126,6 +178,8 @@ const Dashboard = function () {
     const handleDrawerClose = () => {
         setOpen(false);
     };
+
+    const Content = pages[page];
 
     return (
         <>
@@ -138,15 +192,43 @@ const Dashboard = function () {
                     [classes.hide]: open,
                 })}
             >
-                <MenuIcon />
+                <MenuOutlined />
             </IconButton>
             <Drawer variant="persistent" anchor="left" open={open}>
                 <div className={clsx(classes.iconContainer)}>
                     <img src={AutheliaTitleIcon} className={clsx(classes.icon)} />
                 </div>
 
-                <DrawerElement page={Page.Dashboard} active={true} />
+                <DrawerElement
+                    page={Page.Dashboard}
+                    active={page === Page.Dashboard}
+                    onClick={() => {
+                        setPage(Page.Dashboard);
+                    }}
+                />
+                <DrawerElement
+                    page={Page.Users}
+                    active={page === Page.Users}
+                    onClick={() => {
+                        setPage(Page.Users);
+                    }}
+                />
+                <DrawerElement
+                    page={Page.ACL}
+                    active={page === Page.ACL}
+                    onClick={() => {
+                        setPage(Page.ACL);
+                    }}
+                />
+                <DrawerElement
+                    page={Page.Config}
+                    active={page === Page.Config}
+                    onClick={() => {
+                        setPage(Page.Config);
+                    }}
+                />
             </Drawer>
+            <Content />
         </>
     );
 };
